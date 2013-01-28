562edf5e5b28941e36abc014e9613e96b0dfe95b
meawoppl@gmail.com <meawoppl@gmail.com>
2013-01-27 21:45:56 -0800

    Somewhat working.  The intersection operator still has doofed surface normals. . . .

--- csg.py
+++ csg.py
@@ -1,7 +1,7 @@
 import itertools
 import numpy as np
 
-#CSG Library for Python
+# CSG Library for Python
 # Modeled after: https://github.com/evanw/csg.js/blob/master/csg.js
 
 def unit(array):
@@ -41,6 +41,7 @@ class Plane(object):
 
     def flip(self):
         self.normal *= -1
+        self.w *= -1
 
     def copy(self):
         return Plane(self.normal, self.w)
@@ -66,7 +67,7 @@ class Plane(object):
                 
             polyType |= typ
             vertTypes.append(typ)
-
+        
         # Return lists . . .
         cf, cb, f, b = [], [], [], []
 
@@ -106,12 +107,8 @@ class Plane(object):
                     # Calculate where it hits it
                     t = (self.w - self.normal.dot(vi.pos)) / (self.normal.dot(vj.pos - vi.pos))
                     v = vi.interpolate(vj, t)
-                    
-                    print "The edge between", vi
-                    print "\tand", vj
-                    print "\tis cut.", v
-                    
-                    # Add that new vertex to the front and back vertex lists
+                                
+                    # Add that new vertex to both the front and back vertex lists
                     newFront.append( v.copy() )
                     newBack.append(  v.copy() )
 
@@ -123,9 +120,12 @@ class Plane(object):
         return cf, cb, f, b
 
 class Polygon(object):
-    def __init__(self, vertices, shared=None):
-        self.vertices = vertices
-        self.shared = shared
+    def __init__(self, vertices, shared={}, color=None):
+        self.vertices = vertices + []
+        self.shared = shared.copy()
+        if (not color) and ("color" not in shared): 
+            self.shared["color"] = (0.5, 0.5, 0.5) # Grey if otherwise unlabeled
+
         self.plane = Plane.fromPoints(vertices[0].pos, vertices[1].pos, vertices[2].pos)
 
     def fromArray(ndarray):
@@ -148,15 +148,21 @@ class Polygon(object):
 
 class UniqueVertexCollector(object):
     def __init__(self):
+        # Bidirectional dictionary.  
+        # ndarray -> vertexID and vice versa
         self.vertexIndex = {}
         self.indexVertex = {}
+
+        # Internal counter for vertex insertion
         self.indexCount = 0
 
     def addVertex(self, vertex):
+        # ndarrays are not hashable, but their string rep is
         stringRep = vertex.tostring()
         if stringRep in self.vertexIndex:
             return
         
+        # The bidirectional dict (2 dicts)
         self.vertexIndex[stringRep] = self.indexCount
         self.indexVertex[self.indexCount] = vertex
         self.indexCount += 1
@@ -170,24 +176,52 @@ class UniqueVertexCollector(object):
     def __len__(self):
         return len(self.vertexIndex)
 
+    @staticmethod
+    def fromPolygonList(lst):
+        vc = UniqueVertexCollector()
+        for polygon in lst:
+            for vertex in polygon.vertices:
+                vc.addVertex(vertex.pos)
+        return vc
+        
+
+
+
 
 class PolygonMesh(object):
-    def __init__(self, polygonList):
+    def __init__(self, polygonList = []):
         self.polygons = polygonList
 
+    def writeOBJ(self, filename):
+        f = open(filename, "w")
+        f.write("# Created by pyCSG\n")
+
+        # Get the unique vertices
+        vc = UniqueVertexCollector.fromPolygonList(self.polygons)
+
+        # Write the vertices (in order!)
+        for indx in xrange(len(vc)):
+            f.write("v %f %f %f\n" % tuple(vc.getVertex(indx)))
+
+        # Write the face indices
+        for polygon in self.polygons:
+            # .obj is 1 based hence the +1
+            vIndices = [str(vc.getIndex(v.pos) + 1) for v in polygon.vertices]
+            f.write( "f " + " ".join(vIndices) + "\n" )
+
+        f.close()
+            
     # TODO!
     def writeSTL(self, filename):
         pass
+
     def writePLY(self, filename):
         # Go through, and determine the unique vertices
-        vc = UniqueVertexCollector()
-        for polygon in self.polygons:
-            for vertex in polygon.vertices:
-                vc.addVertex(vertex.pos)
-
+        vc = UniqueVertexCollector.fromPolygonList(self.polygons)
 
         vertexCount = len(vc)
         faceCount   = len(self.polygons)
+
         # Open the file and stary writing the header
         f = open(filename, "w")
         f.write("ply\n")
@@ -199,47 +233,75 @@ class PolygonMesh(object):
     
         f.write("element face %i\n" % faceCount )
         f.write("property list uint8 int32 vertex_index\n")
+        for clr in ["red", "blue", "green"]:
+            f.write("property uchar %s\n" % clr)
+
         f.write("end_header\n")
 
         # Write the vertices
         for indx in xrange(len(vc)):
-            f.write( "%f %f %f\n" % tuple(vc.getVertex(indx)) )
+            vtx = vc.getVertex(indx)
+            f.write( "%f %f %f\n" % tuple( vtx ) )
         
         # Write the indices
         for poly in self.polygons:
             nVert = len(poly.vertices)
             indxs = [vc.getIndex(vert.pos) for vert in poly.vertices]
-            f.write(str(nVert) + " " + " ".join([str(i) for i in indxs]) + "\n")
+            count_nIndexes = str(nVert) + " " + " ".join([str(i) for i in indxs])
+            # Cast the 0-1 color into uint8 land, and stringify
+            colors = "%i %i %i" % tuple([int(clr*255) for clr in poly.shared.get("color", (0.5, 0.5, 0.5) )])
+            f.write(count_nIndexes + " " + colors + "\n")
 
 
         print "Wrote {0} vertices, and {1} faces to file '{2}'".format(vertexCount, faceCount, filename)
         f.close()
 
 
-    def __add__(self, otherMesh):
-        t1 = BSPNode(   self.polygons + []    )
-        t2 = BSPNode( otherMesh.polygons + [] )
-        print "len0", len(t1.allPolygons()), len(t2.allPolygons())
-
+    def __or__(self, otherMesh):
+        # Build trees with copies of the two polygon sets
+        t1 = BSPNode(   self.polygons  )
+        t2 = BSPNode( otherMesh.polygons )
         t1.clipTo(t2)
-        print "len1", len(t1.allPolygons()), len(t2.allPolygons())
-
-        # t2.clipTo(t1)
-        # print "len3", len(t1.allPolygons()), len(t2.allPolygons())
-
-        # t2.invert()
-        # print "len4", len(t1.allPolygons()), len(t2.allPolygons())
-
-        # t2.clipTo(t1)
-        # print "len5", len(t1.allPolygons()), len(t2.allPolygons())
-
-        # t2.invert()
-        # print "len6", len(t1.allPolygons()), len(t2.allPolygons())
-        
-        # t1.build(t2.allPolygons())
-        # print "len7", len(t1.allPolygons()), len(t2.allPolygons())
+        t2.clipTo(t1)
+        t2.invert()
+        t2.clipTo(t1)
+        t2.invert()
+        t1.build(t2.allPolygons())
 
         return PolygonMesh(t1.allPolygons())
+
+    def __and__(self, otherMesh):
+        # MRG TODO: not workign 100% correctly.
+        t1 = BSPNode(   self.polygons    )
+        t2 = BSPNode( otherMesh.polygons )
+        t1.invert()
+        t2.clipTo(t1)
+        t2.invert()
+        t1.clipTo(t2)
+        t2.clipTo(t1)
+        t1.build(t2.allPolygons())
+        t1.invert()
+        return PolygonMesh(t1.allPolygons())
+
+
+    def __sub__(self, otherMesh):
+        t1 = BSPNode(self.polygons );
+        t2 = BSPNode(otherMesh.polygons );
+        
+        t1.invert();
+        t1.clipTo(t2)
+        t2.clipTo(t1)
+        t2.invert()
+        t2.clipTo(t1)
+        t2.invert()
+        t1.build(t2.allPolygons())
+        t1.invert()
+
+        return PolygonMesh(t1.allPolygons())
+
+
+        
+
 
 
 class BSPNode(object):
@@ -247,9 +309,9 @@ class BSPNode(object):
         self.plane = None
         self.front = None
         self.back  = None
-        self.polygons = []
+        self.polygons =  [] 
 
-        if len(polygons) != 0: self.build(polygons)
+        if len(polygons) != 0: self.build(polygons + [])
 
     def invert(self):
         # Flip all the polygons on this node
@@ -267,63 +329,125 @@ class BSPNode(object):
 
 
     def allPolygons(self):
+        '''Return a shallow copy to all the polygons upstream of this node.'''
         p = []
         if self.front: p += self.front.allPolygons()
         if self.back:  p += self.back.allPolygons()
-
         return  self.polygons + p
 
     
     def clipTo(self, otherBSP):
+        # My new polygons are those clipped against the tree passed
         self.polygons = otherBSP.clipPolygons(self.polygons)
 
         # Trim the front and back as well
         if self.front: self.front.clipTo(otherBSP)
         if self.back:  self.back.clipTo(otherBSP)
 
-
     def clipPolygons(self, polygons):
         # If a node lacks a splitting plane, then it is a leaf, so no clipping
         if not self.plane: return polygons[:]
         
         fr, bk = [], []
+        # For each polygon
         for polygon in polygons:
+            # Split the polygon by my plane
             cFront, cBack, front, back =  self.plane.splitPolygon(polygon)
             fr += cFront + front
             bk += cBack  + back
 
+        # The generated polygons that are in front of 
+        # me might need to further be clipped by other obects in front of me
         if self.front: 
             fr = self.front.clipPolygons(fr)
-        if self.back : 
+        if self.back: 
             bk = self.back.clipPolygons(bk)
-        
+        else:
+            # This is the actual clip.  
+            # The polygons were partitioned above, 
+            # but now there is nothing behind me in the BPS they can be discarded
+            bk = []
+
         return fr + bk
 
         
     def build(self, polygons):
         if len(polygons) == 0: return
 
+        # Assign this node a cutting plane if it does not already exist.
         if not self.plane: self.plane = polygons[0].plane.copy()
 
+        # Collect polygons that go in back and front of this plane
         newFront = []
         newBack  = []
         for polygon in polygons:
+            # Split (or just allocate) the polygon into coplanar/front/back parts 
             cFront, cBack, front, back = self.plane.splitPolygon(polygon)
+            
+            # Collect the coplanar ones on this node
             self.polygons += cFront + cBack
+
+            # Pile up the front and back ones
             newFront += front
             newBack  += back
             
-        if len(front) != 0 and not self.front:
-            self.front = BSPNode()
+        if len(newFront) != 0:
+            # Init a new node if necessary
+            if not self.front: self.front = BSPNode()
+            
+            # Push Polys up the tree
             self.front.build(newFront)
 
-        if len(back) != 0 and not self.back:
-            self.back = BSPNode()
+        if len(newBack) != 0:
+            # Init a new node if necessary
+            if not self.back: self.back = BSPNode()
+
+            # Push Polys up the tree
             self.back.build(newBack)
 
 
-def makeCircle(center, radius, nTheta):
-    pass
+def makeSphere(center, radius, nTheta, nPhi):
+    phis   = np.linspace(-np.pi/2, np.pi/2, nPhi)
+    thetas = np.linspace(0, 2*np.pi, nTheta)
+
+    def computePos(theta, phi, radius):
+        x = np.cos(theta) * np.cos(phi) * radius
+        y = np.sin(theta) * np.cos(phi) * radius
+        z = np.sin(phi) * radius
+        return np.array([x, y, z], dtype=np.float64)
+
+    polyCollection = []
+    for phiIndx in range(nPhi)[:-1]:
+        for thetaIndx in range(nTheta)[:-1]:
+            theta1 = thetas[thetaIndx]
+            phi1   = phis  [phiIndx]
+
+            theta2 = thetas[thetaIndx+1]
+            phi2   = phis  [phiIndx+1]
+
+            p1 = computePos(theta1, phi1, radius) + center
+            p2 = computePos(theta2, phi1, radius) + center
+            p3 = computePos(theta1, phi2, radius) + center
+            p4 = computePos(theta2, phi2, radius) + center
+
+            v1 = Vertex( p1, unit(p1) )
+            v2 = Vertex( p2, unit(p2)  )
+            v3 = Vertex( p3, unit(p3)  )
+            v4 = Vertex( p4, unit(p4)  )
+
+            if (np.allclose(v1.pos, v2.pos) or np.allclose(v2.pos, v3.pos) or np.allclose(v1.pos, v2.pos)  ): 
+                pass
+            else:
+                polyCollection.append( Polygon([v1, v2, v3], color=(1,0,0)) )
+
+            if (np.allclose(v3.pos, v2.pos) or np.allclose(v2.pos, v4.pos) or np.allclose(v3.pos, v4.pos)  ): 
+                pass
+            else:
+                polyCollection.append( Polygon([v3, v2, v4], color=(1,0,0)) )
+
+
+    return PolygonMesh(polyCollection)
+            
 
 
 def makeTetrahedron(center):
@@ -353,15 +477,27 @@ def makeTetrahedron(center):
     return PolygonMesh(polys)
 
 
+def testOperators(m1, m2, name):
+    sup = PolygonMesh(m1.polygons + m2.polygons)
+    sup.writePLY(name + "-super.ply")
+    sup.writeOBJ(name + "-super.obj")
+
+    union = m1 | m2
+    union.writePLY(name + "-union.ply")
+    union.writeOBJ(name + "-union.obj")
+
+    sub = m1 - m2
+    sub.writePLY(name + "-sub.ply")
+    sub.writeOBJ(name + "-sub.obj")
+
+    intersect = m1 & m2
+    intersect.writePLY(name + "-int.ply")
+    intersect.writeOBJ(name + "-int.obj")
 
 
 if __name__ == "__main__":
-    t1 = makeTetrahedron(np.array([0.0,0.0,0.0]))
-    t2 = makeTetrahedron(np.array([0.0,0.0,-0.5]))
+    t5 = makeSphere(np.zeros(3), 1.0, 15, 15)
+    t6 = makeSphere(np.ones(3), 1, 15, 15)
 
-    t3 = t1 + t2
-
-    t3.writePLY("unioned.ply")
-
-    t4 = PolygonMesh( t1.polygons + t2.polygons )
-    t4.writePLY("superimposed.ply")
+    testOperators(t5, t6, "sphere")
+    
